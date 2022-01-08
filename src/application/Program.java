62937c1f66f20180da4aa57e1a756359ce255e35
Edarcega <edimar.eds@gmail.com>
2022-01-08 15:48:08 -0300

    SellerDaoJDBC and DAOFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import medel.entities.Department;
 import medel.entities.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 
 public class Program {
 
@@ -12,6 +14,7 @@ public class Program {
 		
 		Seller seller = new Seller(21, "Bob", "bobo@gmail.com", new Date(), 3000.0, obj);
 		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(obj);
 		System.out.println(seller);
