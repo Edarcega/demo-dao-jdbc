15a3530c26903f5460bb9659f5eff0ab33563ca2
Edarcega <edimar.eds@gmail.com>
2022-01-08 14:34:52 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,20 @@
 package application;
 
+import java.util.Date;
+
 import medel.entities.Department;
+import medel.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
+		
+		Seller seller = new Seller(21, "Bob", "bobo@gmail.com", new Date(), 3000.0, obj);
+		
+		
 		System.out.println(obj);
+		System.out.println(seller);
 
 	}
 
