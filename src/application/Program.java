7a68a53229bb6184134f312d14fe3b8bf5811ac4
Edarcega <edimar.eds@gmail.com>
2022-01-19 07:58:41 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -30,12 +30,19 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+
 		System.out.println("\n=== TESTE 4: seller insert ===");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
-		
+
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+
+		System.out.println("\n=== TESTE 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+
 	}
 
 }
